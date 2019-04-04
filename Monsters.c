768bed10b1252b7dfc226ea897d79e74891d6e84
Rachel <38117365+rrettig98@users.noreply.github.com>
2019-04-03 20:14:58 -0400

    started the code

---
+++ Monsters.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//MAIN ========================================================================
+int main()
+{	
+	//FUNCTION PROTOTYPES =====================================================
+	void insertAlly(); //add a new ally
+	void removeAlly(); //remove an existing ally
+	void peekAllies(); //view all allies
+	void sizeAlly(); //view the number of allies
+	void isEmptyAlly(); //see if ally linked list is empty
+	int randomNum(); //returns a random number from the API
+	
+	//MONSTER STRUCTURES ======================================================
+	typedef struct Ememy
+	{
+		char* name;
+	} Enemy;
+	
+	typedef struct Ally
+	{
+		char* name;
+		struct Ally* next;
+	} Ally;
+	
+	typedef struct AllyList
+	{
+		Ally* head;
+	} AllyList;
+	
+	//VARIABLES ===============================================================
+	Enemy enemies[4] = {NULL};
+	AllyList* allies = (AllyList*)malloc(sizeof(AllyList));
+	
+	//EXECUTION START =========================================================
+	printf("Hello!");
+}
