c566ef72146fdb7f81c7d1f6914c86a3382dc06a
Rachel <38117365+rrettig98@users.noreply.github.com>
2019-04-10 10:48:12 -0400

    added function

--- c-monsters.c
+++ c-monsters.c
@@ -26,12 +26,12 @@ int main () {
 	//FUNCTION PROTOTYPES = = = = = = = = = = = =
 	void welcomeMessage (char*);
 	
-	monster* insertAlly (monster*); //add a new ally
-	monster* removeAlly (monster*); //remove a preexisting ally
-	monster* peekAllies (monster*); //view all allies
+	monster* fillPlayerRoster (monster*); //add a monster to player roster
+	//monster* removeAlly (monster*); //remove a preexisting ally
+	//monster* peekAllies (monster*); //view all allies
 	
-	int sizeAlly (monster*); //view the number of allies
-	int isEmptyAlly (monster*); //see if ally linked list is empty
+	//int sizeAlly (monster*); //view the number of allies
+	//int isEmptyAlly (monster*); //see if ally linked list is empty
 	int randomNum (); //returns a random number from the API
 	
 	monster* fileIO ();
@@ -59,6 +59,8 @@ int main () {
 	welcomeMessage(playerName);
 	availableMonsterList = fileIO();
 	printList(availableMonsterList);
+	playerRoster = fillPlayerRoster(availableMonsterList);
+	printList(playerRoster);
 	//END OF: Game Code
 	
 } //END OF: main
@@ -149,12 +151,16 @@ monster* fileIO () {
 
 //FUNCTION: printList = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 void printList(monster* n) {
+	int index = 1;
+	printf("\nThe available Monsters are...\n\n");
 	while(n != NULL) {
+		printf("%d ---- \t", index);
     	printf("Name: %s", n -> name);
-    	printf("Attack: %d\n", n -> attack);
-    	printf("Defence: %d\n", n -> defence);
-    	printf("Speed: %d\n", n -> speed);
+    	printf("\tAttack: %d\n", n -> attack);
+    	printf("\tDefence: %d\n", n -> defence);
+    	printf("\tSpeed: %d\n", n -> speed);
 		printf("\n");
+		index ++;
     	n = n -> next;
 	}
 	printf("\n");
@@ -162,11 +168,79 @@ void printList(monster* n) {
 
 
 
-//FUNCTION: insertAlly  = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+//FUNCTION: fillPlayerRoster  = = = = = = = = = = = = = = = = = = = = = = = = =
+monster* fillPlayerRoster (monster* availableMonsters)
+{
+	//variables
+	monster* head = NULL; //player list pointer to return
+	int index; //variable to store user index choice
+	int count = 0; //variable to keep track of number of added monsters
+	
+	printf("Please enter the numbers of the Monsters to add to your roster.\n");
+	printf("You may choose between 1 and 6 Monsters.\n");
+	printf("Enter '0' when you are done.\n\n");
+	
+	while (1 == 1)
+	{
+		//read user input
+		printf("Index: ");
+		scanf(" %d",&index);
+		
+		//break if 0 was entered to quit
+		if (index == 0)
+		{
+			//return if 0 monsters have been entered
+			if (count == 0)
+			{
+				printf("\nYou have not added any Monsters!\n\n");
+				printf("Index: ");
+				scanf(" %d",&index);
+			}
+			else 
+				break;
+		}
+		
+		//check for max of 6 monsters
+		if (count == 6)
+		{
+			printf("\nYou have reached the maximum of 6 monsters.\n");
+			break;
+		}
+		
+		//else save the entered monster to the list
+		monster* temp = (monster*) malloc(sizeof(monster)); //node to store data
+		int i = 1; //int to compare with desired index
+		
+		//cycle through imported list
+		while(availableMonsters != NULL) {
+			//when at the desired monster
+			if (i == index)
+			{
+				//copy data to node 
+				strcpy(temp->name, availableMonsters -> name);
+				temp -> attack = availableMonsters -> attack;
+				temp -> defence = availableMonsters -> defence;
+				temp -> speed = availableMonsters -> speed;
+				
+				//add to front of list
+				temp -> next = head;
+				head = temp;
+				
+				//increase count and break
+				count++;
+				printf("\t%s \twas added to your roster.\n\n",temp -> name);
+				break;
+			}
+			//continue cycling through if not at desired monster
+			availableMonsters = availableMonsters -> next;
+			i++;
+		}
+	}
+	return head;
+}
 
 
-
-//FUNCTION: removeAlly  = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+//FUNCTION: fillPlayerRoster  = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
 
